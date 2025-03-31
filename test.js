38169273fac36cb553d5f224abbeb24b0cc187d1
ishaanchip <ishaanchiplunkar@gmail.com>
2025-03-31 19:21:42 -0400

    simple 1d ai bot

--- test.js
+++ test.js
@@ -1,1 +1,36 @@
-console.log('woah')
+//WELCOME TO AI CLUB
+
+//essential imports & boilerplate to use ai [run npm i openai dotenv]
+    const OpenAI = require("openai")
+    require('dotenv').config();
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
+//lesson #1b: algebra tutor (in js)
+
+async function askTutor (question){
+    try{
+        const response = await openai.chat.completions.create({
+            model:"gpt-4o",
+            messages:[
+            {
+                    "role":"user",
+                    "content":[
+                        {
+                            "type":"text",
+                            "text":"what is 2 + 2?"
+                        }
+                    ]
+            }
+            ]
+        })
+
+        console.log(response.choices[0].message.content)
+    }
+    catch(err){
+        console.log(`there was an error talking to the ai...`)
+    }
+
+}
+askTutor('fda');
